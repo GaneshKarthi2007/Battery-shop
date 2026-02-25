9b86e11d11d633acd542f3791907b3824583e831
harisathees <tsharisathees@gmail.com>
2026-02-25 22:07:55 +0530

    error fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-echo "Clearing config & cache..."
-php artisan config:clear
+echo "Clearing config..."
+php artisan config:clear || true
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate --force || true
 
 echo "Running seeders..."
-php artisan db:seed --force
+php artisan db:seed --force || true
 
 echo "Starting Laravel..."
 exec php artisan serve --host=0.0.0.0 --port=$PORT
